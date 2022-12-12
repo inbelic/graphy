688b08029e5b064ecde2a2eac7a30d1ddec14da2
inbelic <canadienfinn@gmail.com>
2022-12-12 21:41:36 +0100

    [Graph] some touch ups

--- Graph/Graph.hs
+++ Graph/Graph.hs
@@ -1,5 +1,7 @@
 module Graph where
 
+import Data.List (delete)
+
 {- There exists 3 implementations of a graph that we will consider. A graph
     using adjacency lists, adjacency matrix and a linked network
 -}
@@ -61,25 +63,53 @@ removeEdge e = Graph . map (rmvEdge e) . internal
       | otherwise = Node k ks val
       where (u,v) = edge e
 
+
+-- check if a graph is simple
+-- O(|e|)
 isSimple :: Graph a -> Bool
 isSimple = all isSimple' . internal
   where
     isSimple' :: Node a -> Bool
     isSimple' (Node k ks _) = (isUnique k ks) && (not . elem k $ ks)
 
+    -- uses the assumption of a sorted list
     isUnique :: (Eq a) => a -> [a] -> Bool
     isUnique _ [] = True
     isUnique prev (x:xs)
       | prev == x = False
       | otherwise = isUnique x xs
 
+-- get the underlying simple graph of a graph
+-- O(|e|)
+simple :: Graph a -> Graph a
+simple = Graph . map simple' . internal
+  where
+    simple' :: Node a -> Node a
+    simple' (Node k ks val) =
+      let ks' = snd . foldr unique (k, []) . filter (/= k) $ ks
+       in Node k ks' val
+
+    -- uses the assumption of a sorted list
+    unique :: (Eq a) => a -> (a, [a]) -> (a, [a])
+    unique x (prev, xs)
+      | x == prev = (x, xs)
+      | otherwise = (x, x:xs)
+
 -- given the ordered nature of edges we can simply check if graphs are identical
 instance (Eq a) => Eq (Graph a) where
   (==) (Graph g) (Graph h)
     = all (uncurry (==)) . zip g $ h
 
+-- determine if two graphs are identical
+-- O(|e| + |v|)
 isIdentical :: (Eq a) => Graph a -> Graph a -> Bool
 isIdentical g h = g == h
 
-isIsomorphic :: Graph a -> Graph a -> Bool
-isIsomorphic _ _ = undefined
+-- determine if two graphs are isomorphic
+-- maybe we can write a function that will reconstruct a graph in a consistent
+-- way. Then we can just check if the reconstructions are identical
+isIsomorphic :: (Eq a) => Graph a -> Graph a -> Bool
+isIsomorphic g h = isIdentical (reconstruct g) (reconstruct h)
+
+reconstruct :: Graph a -> Graph a
+reconstruct = undefined
